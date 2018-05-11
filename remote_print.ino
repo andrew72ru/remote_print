a09071f3a6124777f4d300209ad92580aaa76506
Андрей Ждановских <andrew72ru@gmail.com>
2018-05-11 13:58:36 +0500

    Right way

--- remote_print.ino
+++ remote_print.ino
@@ -9,37 +9,6 @@ Adafruit_Thermal printer(&printerSerial);
 // ------- Adafruit Thermal to serial port ------- //
 
 
-// ------------- GPRS init variables ------------ //
-//SoftwareSerial gsm(7, 8); // RX, TX
-//String Arsp, Grsp, Helper;
-// ------------- GPRS init variables ------------ //
-
-// void setup() {
-//  Serial.begin(9600);
-//  Serial.println("SETUP");
-//  gsm.begin(9600);
-//  gsm.println("AT+CMGF=1");
-//  delay(1000);
-//  gsm.println("AT+CNMI=1, 2, 0, 0, 0");
-// }
-
-  /*
-void loop() {
-  if (gsm.available()) {
-    Grsp = gsm.readString();
-    
-    Serial.println(Grsp);
-    
-    if(Grsp.substring(1,6) == "+CMT") {
-      Serial.println("Hei! I Receive an SMS!");
-    } else {
-      Serial.println("Substring is");
-      Serial.println(Grsp.substring(1,6));
-    }
-//    PrintSms(Grsp);
-  }
-}
-  */
 
 void PrintSms(String str) {
   printerSerial.begin(9600);
